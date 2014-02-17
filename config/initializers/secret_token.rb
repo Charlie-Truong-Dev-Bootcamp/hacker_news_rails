9e7880b9bf2e773b0166620fe2cb4e8516ebc67a
Daniel Lu <dandydanny@gmail.com>
2014-02-17 13:35:16 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackerNews::Application.config.secret_key_base = '5a43efce760321d9b4a015001493d80bd1673581421e24088d9d215c86adf2de3eae63deac4dbad946bd872ea6ab37b882a31980f9f539fd19b9e6c55c9d0096'
