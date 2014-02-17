0d1961a7ab697c2e65b19631da87ed9fa1a687f2
CharlieTruong <charlie.m.truong@gmail.com>
2014-02-17 15:26:30 -0600

    complete log-in form and controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect :root
+      redirect_to :root
     else
       flash.now.alert = "Invalid Email or Password"
       render "new"
