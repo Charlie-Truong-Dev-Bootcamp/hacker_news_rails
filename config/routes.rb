bbd2faa3421145cd596c39e423271cedb830f589
CharlieTruong <charlie.m.truong@gmail.com>
2014-02-17 14:12:30 -0600

    Complete routes for the site

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 HackerNews::Application.routes.draw do
+  resources :users, only: [:new, :create, :show] do
+    resources :comments, only: [:index]
+    resources :posts, only: [:index]
+  end
 
+
+
+  resources :posts, only:[:new, :index, :create] do
+    resources :votes, only: [:create]
+    resources :comments, only: [:create, :index]
+  end
 end
