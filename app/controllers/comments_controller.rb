bc2852542255f3056bc06128d2d964dac9d00b24
CharlieTruong <charlie.m.truong@gmail.com>
2014-02-17 14:45:57 -0600

    flesh out CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,30 @@
 class CommentsController < ApplicationController
+  before_filter :determine_scope :except => [:create]
+
   def index
+    @comments = @scope.all
   end
 
   def create
+    comment = Comment.create(post_params)
+    user = current_user
+    post = Post.find(params[:post_id])
+    user.comments << comment
+    post.comments << comment
+    redirect_to post_comments(post)
+  end
+
+  private
+
+  def determine_scope
+    if params[:user_id]
+      @scope = User.find(params[:user_id]).comments
+    else
+      @scope = Post.find(params[:post_id]).comments
+    end
+  end
+
+  def post_params
+    params.require(:comment).permit(:text)
   end
 end
